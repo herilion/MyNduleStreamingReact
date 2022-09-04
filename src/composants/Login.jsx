755c074bfbd2377c28930edd754c83bb6356125d
herilion <heritierlionge@yahoo.com>
2022-09-04 20:13:07 +0200

    update code

--- src/composants/Login.jsx
+++ src/composants/Login.jsx
@@ -1,9 +1,10 @@
 import '../styles/accueilStyle.css'
 import music from '../assets/music.png'
 import React from 'react';
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import accessUrl from '../composants/spotifyThings'
 import GoogleLogin from 'react-google-login';
-// import axios from 'axios';
+
 const Login = () => {
     // =======Google=========
     const clienId = '1045931220842-nu223q4a2lg6sqno0aamg3riesl6jjtu.apps.googleusercontent.com'
@@ -12,9 +13,6 @@ const Login = () => {
             ? JSON.parse(localStorage.getItem('loginData'))
             : null
     )
-    // const handleFailure = (result) => {
-    //     alert(result);
-    // };
     const handleLogin = async (googleData) => {
         const res = await fetch('/api/google-login', {
             method: POST,
@@ -34,61 +32,6 @@ const Login = () => {
         setLoginData(null);
     }
     // =======Google=========
-    // =======connect to spotify api start=========
-    const clientId = 'efed7b8e30ee495f9fe5fe8a6dd5fbda';
-    const redirectUri = "http://localhost:5173/";
-    const authEndPoint = "https://accounts.spotify.com/authorize";
-    const responseType = "token";
-
-    const [token, setToken] = useState("")
-    // const [searchKey, setSearchKey] = useState("")
-    // const [artists, setArtists] = useState([])
-
-    useEffect(() => {
-        const hash = window.location.hash
-        let token = window.localStorage.getItem("token")
-
-        // getToken()
-
-
-        if (!token && hash) {
-            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-
-            window.location.hash = ""
-            window.localStorage.setItem("token", token)
-        }
-
-        setToken(token)
-
-    }, [])
-    const logout = () => {
-        setToken("")
-        window.localStorage.removeItem("token")
-    }
-    // const searchArtists = async (e) => {
-    //     e.preventDefault()
-    //     const { data } = await axios.get("https://api.spotify.com/v1/search", {
-    //         headers: {
-    //             Authorization: `Bearer ${token}`
-    //         },
-    //         params: {
-    //             q: searchKey,
-    //             type: "artist"
-    //         }
-    //     })
-
-    //     setArtists(data.artists.items)
-    // }
-
-    // const renderArtists = () => {
-    //     return artists.map(artist => (
-    //         <div key={artist.id}>
-    //             {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt="" /> : <div>No Image</div>}
-    //             {artist.name}
-    //         </div>
-    //     ))
-    // }
-    // =======connect to spotify api and=========
     return (
         <section className="container">
             <div>
@@ -104,21 +47,7 @@ const Login = () => {
                 <div><input placeholder='Votre mot de passe' type="text" className='input' /></div>
             </div>
             <div className='buttonContain'>
-                {!token ?
-                    <a href={`${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}`} className='button'>Log in</a>
-                    : <button onClick={logout}>Logout</button>
-                }
-                {/* {token ?
-                    <form onSubmit={searchArtists}>
-                        <input type="text" onChange={e => setSearchKey(e.target.value)} />
-                        <button type={"submit"}>Search</button>
-                    </form>
-
-                    : <h2>Please login</h2>
-                }
-
-                {renderArtists()} */}
-
+                <a href={accessUrl} className='button'>Log in</a>
             </div>
             <div>
                 {
@@ -133,9 +62,7 @@ const Login = () => {
                                 clientId={clienId}
                                 buttonText="Log in with Google"
                                 onSuccess={handleLogin}
-                                // onFailure={handleFailure}
                                 cookiePolicy={'single_host_origin'}>
-
                             </GoogleLogin>
                         )
                 }
