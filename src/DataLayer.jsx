87b447cf967b8b0406f1087def61179e4498ea82
herilion <heritierlionge@yahoo.com>
2022-09-05 09:47:27 +0200

    creation de datelayer

---
+++ src/DataLayer.jsx
@@ -0,0 +1,6 @@
+import React,
+{
+    createContext,
+    useContext,
+    useReducer
+} from "react";
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DataLayer initialState reducer>
+      <App />
+    </DataLayer>
+
   </React.StrictMode>
 )
