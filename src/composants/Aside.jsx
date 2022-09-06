d01fd301e4060e6e3f5998135c4dbd18e0d99146
herilion <heritierlionge@yahoo.com>
2022-09-06 11:19:03 +0200

    get playlists

--- src/composants/Aside.jsx
+++ src/composants/Aside.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/aside.css'
 import music from '../assets/music.png'
 // import { HomeIcon, SearchIcon, LibraryMusicIcon } from '@mui/icons-material';
+import { useDataLayervalue } from '../DataLayer';
 import AsideOption from './AsideOption';
 const Aside = () => {
+    const [{ playlists }, dispatch] = useDataLayerValue()
     return (
         <div className="aside">
             <div className='logotext'>
@@ -14,6 +16,9 @@ const Aside = () => {
             <AsideOption title="Recherche" />
             <AsideOption title="Bibliothèque" />
             <strong>PlayLISTS</strong>
+            {playlists?.items?.map(playlists => (
+                <AsideOption title={playlists.name} />
+            ))}
             <AsideOption title="hip Hop" />
             <AsideOption title="RNB" />
             <AsideOption title="Rock" />
